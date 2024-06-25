7ed2c2ff7a7266128bc6d45411dee5bbb2d16c47
Umadsofi412 <umad.20scse1010395@galgotiasuniversity.edu.in>
2024-06-25 11:08:48 +0530

    "new changes"

--- frontend/src/Components/pages/signup.jsx
+++ frontend/src/Components/pages/signup.jsx
@@ -1,11 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from 'axios';
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Signup = () => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
-    const [error,setError] = useState('')
+    const [error, setError] = useState('')
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
 
@@ -13,39 +13,45 @@ const Signup = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        try{
-            const response = await axios.post('http://localhost:5000/api/auth/signup', {username, email, password});
-            localStorage.setItem('token',response.data.token);
+        try {
+            const response = await axios.post('http://localhost:5000/api/auth/signup', { username, email, password });
+            localStorage.setItem('token', response.data.token);
             navigate('/login');
-        }catch(error){
-            console.error('Error:',error);
+        } catch (error) {
+            console.error('Error:', error);
             setError(error.response ? error.response.data.message : 'Network Error')
         }
     };
-    return(
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="">
-                    Username
-                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
-                </label>
+    return (
+        <form className="Signup" onSubmit={handleSubmit}>
+            <div className="main-signup">
+                <div className="sign-up-header">
+                    <h1>Create Free Account</h1>
+                    <p>Discover Prime Spaces for Your Business – Sign Up Now!</p>
+                </div>
+                <div className="form-display">
+                    <div className="display-content">
+                        <label htmlFor="">
+                            <input type="text" value={username} placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+                        </label>
+                    </div >
+                    <div className="display-content">
+                        <label htmlFor="Email">
+                            <input type="email" placeholder="Email Address" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </label>
+                    </div>
+                    <div className="display-content">
+                        <label htmlFor="Password">
+                            <input type="password" placeholder="Create Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </label>
+                    </div>
+                    <button type="submit">Signup</button>
+                    <p>Already have an account? <a href="#">Sigin</a> </p>
+                </div>
             </div>
-            <div>
-                <label htmlFor="Email">
-                    Email
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                </label>
-            </div>
-            <div>
-                <label htmlFor="Password">
-                    Password
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </label>
-            </div>
-            <button type="submit">Signup</button>
-            {error && <p style={{color:'red'}}>{error}</p>}
+            {error && <p style={{ color: 'red' }}>{error}</p>}
         </form>
-    ) 
- }
+    )
+}
 
- export default Signup
+export default Signup
